d8f098400baf622df12d65b8dacca4b698f28514
Akihito Yokota <akiaki118row@yahoo.co.jp>
2023-12-12 00:02:05 +0900

    Load image from gh

--- __init__.py
+++ __init__.py
@@ -9,10 +9,22 @@ __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
 
 import server
 import json
+import base64
 import aiohttp
 from aiohttp import web
 
 @server.PromptServer.instance.routes.post('/custom_nodes/ComfyGH/queue_prompt')
 async def upload_file(request):
-    server.PromptServer.instance.send_sync("queue_prompt", { "status": "aaaaa" })
-    return web.Response(text="ok")
+    data = await request.json()
+    image_data = data.get('image')
+
+    if image_data:
+        image_data = base64.b64decode(image_data)
+        input_dir = folder_paths.get_input_directory()
+        file_path = os.path.join(input_dir, "comfygh.png")
+        with open(file_path, "wb") as f:
+            f.write(image_data)
+        server.PromptServer.instance.send_sync("queue_prompt", { "status": "aaaaa" })
+        return web.Response(text="ok")
+    
+    return web.Response(text="no image data", status=400)
--- nodes.py
+++ nodes.py
@@ -1,20 +1,40 @@
+import torch
+import folder_paths
+import os
+from PIL import Image, ImageOps
+import numpy as np
+
 
 class LoadImageFromGH:
     @classmethod
     def INPUT_TYPES(s):
+        input_dir = folder_paths.get_input_directory()
+        files = ["comfygh.png"]
         return {
             "required": {
-                "raw_text": ("STRING", {"multiline": True})
+                "image": (sorted(files), {"image_upload": False})
             }
         }
-    RETURN_TYPES = ("STRING", )
-    RETURN_NAMES = ("output", )
+    RETURN_TYPES = ("IMAGE", )
+    RETURN_NAMES = ("image", )
     FUNCTION = "run"
 
     CATEGORY = "ComfyGH"
 
-    def run(self, raw_text):
-        return (raw_text, )
+    def run(self, image):
+        image_path = folder_paths.get_annotated_filepath(image)
+        i = Image.open(image_path)
+        i = ImageOps.exif_transpose(i)
+        image = i.convert("RGB")
+        image = np.array(image).astype(np.float32) / 255.0
+        image = torch.from_numpy(image)[None,]
+        if 'A' in i.getbands():
+            mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
+            mask = 1. - torch.from_numpy(mask)
+        else:
+            mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
+        return (image, mask.unsqueeze(0))
+
 
 
 class SendImageToGH:
