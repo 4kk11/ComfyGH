94eb6545c89e6cf0d13efb5d3546e3fbaf0806f9
Akihito Yokota <akiaki118row@yahoo.co.jp>
2023-12-17 22:40:13 +0900

    Cleanup

--- gh/src/params/Param_ComfyImage.cs
+++ gh/src/params/Param_ComfyImage.cs
@@ -19,11 +19,6 @@ namespace ComfyGH.Params
 {
     public class Param_ComfyImage : GH_Param<GH_ComfyImage>
     {
-        // ビットマップ画像を格納するプライベート変数。
-        private Bitmap _image;
-
-        // 画像のキーを格納するプライベート変数。
-        private int _imageKey;
 
         public Param_ComfyImage() : base(new GH_InstanceDescription("ComfyImage", "CI", "ComfyImage", "ComfyGH", "Params"))
         {
@@ -36,70 +31,26 @@ namespace ComfyGH.Params
 
         public override void ExpireSolution(bool recompute)
         {
-            _image = null;
-            _imageKey++;
             base.ExpireSolution(recompute);
         }
 
-        // キャッシュされたプレビュー画像を取得するメソッド。
-        public ImageUriImageLoading GetCachedPreview(out Bitmap image)
+        public Bitmap LoadPreviewImage()
         {
-            image = _image;
-
-            if (image != null)
-            {
-                return ImageUriImageLoading.FinishedLoading;
-            }
-
-            if (m_data.IsEmpty)
-            {
-                return ImageUriImageLoading.None;
-            }
-
-            // Thread thread = new Thread((ThreadStart)delegate
-            // {
-            //     LoadPreviewImage(_imageKey);
-            // });
-            // thread.IsBackground = true;
-            // thread.Priority = ThreadPriority.BelowNormal;
-            // thread.Start();
-            _ = LoadPreviewImage(_imageKey);
-            return ImageUriImageLoading.BusyLoading;
-        }
-
-
-        // プレビュー画像を読み込むためのメソッド。
-        private async Task LoadPreviewImage(int key)
-        {
-            if (key != _imageKey)
-            {
-                return;
-            }
+            if (m_data.IsEmpty) return null;
 
             List<ComfyImage> list = new List<ComfyImage>();
-
             foreach (IGH_Goo item in m_data.AllData(skipNulls: true))
             {
                 if (item is GH_ComfyImage comfyImage)
                 {
-                    list.Add(new ComfyImage(comfyImage.Value));
+                    list.Add(comfyImage.Value);
                 }
             }
 
-            if (list.Count == 0)
-            {
-                _image = null;
-                return;
-            } 
+            if (list.Count == 0) return null;
 
-            
             Bitmap bitmap = list[0].bitmap;
-
-
-            if (key == _imageKey)
-            {
-                _image = bitmap;
-            }
+            return bitmap;
         }
 
 
