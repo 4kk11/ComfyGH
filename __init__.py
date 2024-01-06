cef605b551d29f64640a70bd8d5382bbd807e2cb
Akihito Yokota <akiaki118row@yahoo.co.jp>
2024-01-06 14:53:55 +0900

    Add: try exept statement

--- __init__.py
+++ __init__.py
@@ -20,48 +20,36 @@ client_id = "0CB33780A6EE4767A5DDC2AD41BFE975"
 async def upload_file(request):
     data = await request.json()
 
+    if data is None:
+        return web.Response(text="no data", status=400)
+
     # data = {id: {type: string, value: any}, ...}
 
-    for id in data.keys():
-        type = data[id]['type']
-        value = data[id]['value']
-        
-        if(type == "GH_LoadImage"):
-            # value -> 画像(base64string)
-            # 画像を保存して、valueをファイル名に変更
-            file_name =  'comfygh_' + id + '.png'
-            image_data = base64.b64decode(value)
-            input_dir = folder_paths.get_input_directory()
-            file_path = os.path.join(input_dir, file_name)
-            with open(file_path, "wb") as f:
-                f.write(image_data)
-            data[id]['value'] = file_name
+    try:
+        for id in data.keys():
+            type = data[id]['type']
+            value = data[id]['value']
+            
+            if(type == "GH_LoadImage"):
+                # value -> 画像(base64string)
+                # 画像を保存して、valueをファイル名に変更
+                file_name =  'comfygh_' + id + '.png'
+                image_data = base64.b64decode(value)
+                input_dir = folder_paths.get_input_directory()
+                file_path = os.path.join(input_dir, file_name)
+                with open(file_path, "wb") as f:
+                    f.write(image_data)
+                data[id]['value'] = file_name
+    except Exception as e: 
+        print(e)
+        return web.Response(text="error", status=400)
         
     server.PromptServer.instance.send_sync("update_gh_loadimage", data)
     server.PromptServer.instance.send_sync("update_gh_text", data)
         
-
-
-    # dataから画像(base64string)を取得
-
-    # dataからテキストを取得
-
-    # image_data = data.get('image')
-    # file_name = nodes.SOURCE_IMAGE_NAME
-
-    # if image_data:
-    #     image_data = base64.b64decode(image_data)
-    #     input_dir = folder_paths.get_input_directory()
-    #     file_path = os.path.join(input_dir, file_name)
-    #     with open(file_path, "wb") as f:
-    #         f.write(image_data)
-    #     server.PromptServer.instance.send_sync("update_preview", { "image": file_name })
-
-    #     server.PromptServer.instance.send_sync("queue_prompt", { })
-    #     return web.Response(text="ok")
+    #server.PromptServer.instance.send_sync("queue_prompt", { })
+    return web.Response(text="ok")
     
-    return web.Response(text="no image data", status=400)
-
 
 @server.PromptServer.instance.routes.post('/custom_nodes/ComfyGH/progress')
 async def propagate_progress(request):
